1b7f17e0eb0dc16daab4ea24b44d7a12b478ed50
Snow-00 <wariorwilly43@gmail.com>
2023-08-09 13:37:45 +0700

    finish add product

--- pkg/controllers/productcontroller/product_controller.go
+++ pkg/controllers/productcontroller/product_controller.go
@@ -5,6 +5,9 @@ import (
 	"html/template"
 	"github.com/Snow-00/my-gocrudweb/pkg/models/productmodel"
 	"github.com/Snow-00/my-gocrudweb/pkg/models/categorymodel"
+	"github.com/Snow-00/my-gocrudweb/pkg/entities"
+	"time"
+	"strconv"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +41,33 @@ func Add(w http.ResponseWriter, r *http.Request) {
 		}
 
 		temp.Execute(w, data)
+	}
+
+	if r.Method == "POST" {
+		var product entities.Product
+
+		categoryId, err := strconv.Atoi(r.FormValue("category_id"))
+		if err != nil {
+			panic(err)
+		}
+
+		stock, err := strconv.Atoi(r.FormValue("stock"))
+		if err != nil {
+			panic(err)
+		}
+
+		product.Name = r.FormValue("name")
+		product.Category.Id = uint(categoryId)
+		product.Stock = int64(stock)
+		product.Description = r.FormValue("description")
+		product.CreatedAt = time.Now()
+		product.UpdatedAt = time.Now()
+
+		if ok := productmodel.Create(product); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusTemporaryRedirect)
+		}
+
+		http.Redirect(w, r, "/products", http.StatusSeeOther)
 	}
 }
 
